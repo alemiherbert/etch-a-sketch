b626593dc366bff78aa3639e44692478823ac879
alemi <alemiherbert@gmail.com>
2023-01-17 06:22:19 +0300

    Added buttons, colors

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 const paintingArea = document.getElementById("painting-area");
+const eraseBtn = document.getElementById("erase-btn");
+const clearBtn = document.getElementById("clear-btn");
+const colorBtn = document.getElementById("color-btn");
 let isDrawing = false;
+let initialColor = "#000";
 
-function createGrid(element, pixelSize) {
-    const numberOfRows = element.offsetHeight / pixelSize;
-    const numberOfColumns = element.offsetWidth / pixelSize;
-    const numberOfElements = numberOfRows * numberOfColumns;
+function createGrid(element, numPixels) {
+    const width = 400;
+    const pixelSize = width / numPixels;
+    const numberOfElements = numPixels * numPixels;
 
     for (let pixelsCreated = 0; pixelsCreated < numberOfElements; pixelsCreated++) {
         const pixel = document.createElement("div");
+        pixel.style.minWidth = pixelSize;
+        pixel.style.minHeight = pixelSize;
         element.appendChild(pixel);
     }
     return [...element.childNodes];
 }
 
 
-const pixels = createGrid(paintingArea, 8);
+const pixels = createGrid(paintingArea, 50);
 
+clearBtn.addEventListener("click", () => {
+    pixels.forEach((pixel) => {
+        pixel.style.backgroundColor = "#fff"
+    })
+})
+
+eraseBtn.addEventListener("click", () => {
+    initialColor = "#fff"
+})
+
+colorBtn.addEventListener("change", () => {
+    initialColor = colorBtn.value
+})
 
 pixels.forEach(function (pixel) {
     pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "red"
+        pixel.style.backgroundColor = initialColor
     })
 })
